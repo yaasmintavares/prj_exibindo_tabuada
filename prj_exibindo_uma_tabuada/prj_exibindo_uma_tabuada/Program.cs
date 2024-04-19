6c717fd80a11649f34f155fc5ef8e7b96543d85b
Yasmin Tavares <134327320+yaasmintavares@users.noreply.github.com>
2024-04-19 14:33:56 -0300

    Add files via upload
    Estrutura de Repetição : Loop ou Laço - Utilizando o comando FOR (Para ... Fim-Para)
    
    Exibindo uma tabuada qualquer na tela com o uso da estrutura Par ... Fim-para (For)

---
+++ prj_exibindo_uma_tabuada/prj_exibindo_uma_tabuada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj_exibindo_uma_tabuada
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c, num;
+            Console.Write("Digite o número para a tabuada: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (c = 1; c <= 10; c++)
+            {
+                Console.WriteLine(num + " X " + c + " = " + (num * c));
+            }
+            Console.WriteLine("\nPress. <ENTER> para Finalizar\n");
+            Console.ReadKey();
+        }
+    }
+}
